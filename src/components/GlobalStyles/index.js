bc580e1031588fe258ceec87b235c660a72d62a0
Justus Burger <justus@leadhome.co.za>
2021-03-11 19:37:41 +0200

    Fix body overflow issue

--- src/components/GlobalStyles/index.js
+++ src/components/GlobalStyles/index.js
@@ -3,5 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyles = createGlobalStyle`
   body {
     background: #f4f4f4;
+    overflow: hidden;
+    max-width: 100%;
   }
 `;
